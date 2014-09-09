38ccd8f869a60509072eb7300482af1a8670a9c9
Outpox <guillaumeoutpox@gmail.com>
2014-09-09 16:39:57 +0200

    Weapons click handling

--- js/weapons.js
+++ js/weapons.js
@@ -1,9 +1,23 @@
 $(function() {
     $('#menu input').click(function() {
-	var $this = $(this);
-	window.console.log($this);
+        var $this = $(this);
+        window.console.log($this);
     });
     $('#primwepbut').click(function() {
-	$('#rifles').toggle(50);
+        $('#rifles').toggle(50);
+    });
+    $('#menu input:radio').addClass('input_hidden');
+
+    $('#menu li').click(function(e) {
+        e.preventDefault();
+        window.console.log("click");
+        if ($(this).closest('li').attr('class') === 'selected') {
+            window.console.log("already selected");
+            $(this).closest('li').removeClass('selected');
+        }
+        else {
+            window.console.log("not selected");
+            $(this).closest('li').addClass('selected').siblings().removeClass('selected');
+        }
     });
 });
