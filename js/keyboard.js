1a0b54167d37f37fbee077f5d5c75a27ba4b2b61
Outpox <guillaumeoutpox@gmail.com>
2014-09-09 21:57:45 +0200

    Console log of color commented

--- js/keyboard.js
+++ js/keyboard.js
@@ -8,14 +8,14 @@ $(function() {
 	if (bgc === "rgb(211, 211, 211)") {
 	    $this.css({'background-color': 'greenyellow'});
 	    selectedkey.html("");
-	    window.console.log(bgc + " -> true");
+//	    window.console.log(bgc + " -> true");
 	}
 	else {
 	    $("#container li").each(function(index) {
 		$(this).css({'background-color': 'greenyellow'});
 	    });
 	    $this.css({'background-color': 'lightgrey'});
-	    window.console.log(bgc + " -> false");
+//	    window.console.log(bgc + " -> false");
 	}
     });
 });
