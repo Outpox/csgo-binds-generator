64aa583e6671f4374467ef28c2ddacfd56ab2e81
Outpox <guillaumeoutpox@gmail.com>
2014-09-09 20:47:29 +0200

    Initial keys input

--- js/weapons.js
+++ js/weapons.js
@@ -1,4 +1,15 @@
 $(function() {
+    var key = $('#selectedkey');
+    var write = $("#write");
+    var pwep = $('input[name=primary]:checked', '#pwep').val();
+    function generate() {
+        window.console.log("generate");
+        window.console.log(key.html());
+        window.console.log(pwep);
+        if (key !== "") {
+            write.html("bind \"" + key.html().toLowerCase() + "\"");
+        }
+    }
     $('#menu input').click(function() {
         var $this = $(this);
         window.console.log($this);
@@ -6,17 +17,19 @@ $(function() {
     $('#primwepbut').click(function() {
         $('#rifles').toggle(50);
     });
+    $('#secondwepbut').click(function() {
+        $('#gun').toggle(50);
+    });
     $('#menu input:radio').addClass('input_hidden');
 
     $('#menu li').click(function(e) {
         e.preventDefault();
-        window.console.log("click");
+        generate();
         if ($(this).closest('li').attr('class') === 'selected') {
             window.console.log("already selected");
             $(this).closest('li').removeClass('selected');
         }
         else {
-            window.console.log("not selected");
             $(this).closest('li').addClass('selected').siblings().removeClass('selected');
         }
     });
