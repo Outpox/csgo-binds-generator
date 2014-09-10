93ff09027fcc9436b575ae76fcbef5f31c8cad3e
Outpox <guillaumeoutpox@gmail.com>
2014-09-11 00:25:25 +0200

    Secondary weapon js added

--- js/weapons.js
+++ js/weapons.js
@@ -2,46 +2,56 @@ $(function() {
     var key = $('#selectedkey');
     var write = $("#write");
     function generate() {
-        var pwep = $("#primaryform :checked").val();
-        window.console.log("Selected key : " + key.html());
-        window.console.log("Selected weapon : " + pwep);
-        if (key.html() !== "") {
-            if (pwep !== undefined) {
-                pwep = "\"" + pwep + "\"";
-            }
-            write.html("bind \"" + key.html().toLowerCase() + "\" " + pwep);
-        }
+	var pwep = $("#primaryform :checked").val();
+	var swep = $("#secondaryform :checked").val();
+	if (pwep === undefined) {
+	    pwep = "";
+	}
+	if (swep === undefined) {
+	    swep = "";
+	}
+//	window.console.log("Selected key : " + key.html());
+//	window.console.log("Selected weapon : " + pwep);
+	if (key.html() !== "") {
+	    if (pwep !== undefined && pwep !== "") {
+		pwep = "\"" + pwep + "\"";
+	    }
+	    if (swep !== undefined && swep !== "") {
+		swep = "\"" + swep + "\"";
+	    }
+	    write.html("bind \"" + key.html().toLowerCase() + "\" " + pwep + swep);
+	}
     }
     $('#primwepbut').click(function() {
-        $('#primary').toggle(100);
+	$('#primary').toggle(100);
     });
     $('#riflesbut').click(function(e) {
-        e.preventDefault();
-        $('#rifles').toggle(100);
+	e.preventDefault();
+	$('#rifles').toggle(100);
     });
     $('#smgsbut').click(function(e) {
-        e.preventDefault();
-        $('#smgs').toggle(100);
+	e.preventDefault();
+	$('#smgs').toggle(100);
     });
     $('#secondwepbut').click(function() {
-        $('#gun').toggle(100);
+	$('#gun').toggle(100);
     });
     $('#menu input:radio').addClass('input_hidden');
 
     $('#menu li').click(function(e) {
-        e.preventDefault();
-        $(this).children().prop("checked", true);
-        generate();
-        if ($(this).closest('li').attr('class') === 'selected') {
-            $(this).closest('li').removeClass('selected');
-            $(this).children().prop("checked", false);
-        }
-        else {
-            $(this).closest('li').addClass('selected').siblings().removeClass('selected');
-        }
+	e.preventDefault();
+	$(this).children().prop("checked", true);
+	if ($(this).closest('li').attr('class') === 'selected') {
+	    $(this).closest('li').removeClass('selected');
+	    $(this).children().prop("checked", false);
+	}
+	else {
+	    $(this).closest('li').addClass('selected').siblings().removeClass('selected');
+	}
+	generate();
     });
     $('#keyboard li').click(function() {
-        if ($(":checked").val() !== undefined)
-            generate();
+	if ($(":checked").val() !== undefined)
+	    generate();
     });
 });
