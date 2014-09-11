7403c9ea8068ef1590b202a500c7dc320df1326b
Outpox <guillaumeoutpox@gmail.com>
2014-09-11 11:41:47 +0200

    Symbol class for special characters

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,21 +1,28 @@
 $(function() {
     var $write = $('#write');
     var selectedkey = $('#selectedkey');
+    var $this;
+    var character;
     $('#keyboard li').click(function() {
-	var $this = $(this), character = $this.html();
-	var bgc = $this.css("background-color");
-	selectedkey.html(character);
-	if (bgc === "rgb(211, 211, 211)") {
-	    $this.css({'background-color': 'greenyellow'});
-	    selectedkey.html("");
+        $this = $(this);
+        if ($this.attr('class') === 'symbol' || $this.attr('class') === 'symbol lastitem') {
+            character = $this.children().html();
+        } else {
+            character = $this.html();
+        }
+        var bgc = $this.css("background-color");
+        selectedkey.html(character);
+        if (bgc === "rgb(211, 211, 211)") {
+            $this.css({'background-color': 'greenyellow'});
+            selectedkey.html("");
 //	    window.console.log(bgc + " -> true");
-	}
-	else {
-	    $("#container li").each(function(index) {
-		$(this).css({'background-color': 'greenyellow'});
-	    });
-	    $this.css({'background-color': 'lightgrey'});
+        }
+        else {
+            $("#container li").each(function(index) {
+                $(this).css({'background-color': 'greenyellow'});
+            });
+            $this.css({'background-color': 'lightgrey'});
 //	    window.console.log(bgc + " -> false");
-	}
+        }
     });
 });
